7f4e9f3a81448b9db995406a2bf2166074d4cb28
Roman <example@gmail.com>
2021-12-12 13:06:48 +0200

    desktop

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var splide = new Splide( '.splide', {
+    type   : 'loop',
+    perPage: 3,
+    focus  : 'center',
+    arrowPath: 'm15.5 0.932-4.3 4.38...',
+    rewind: true
+  } );
+  
+splide.mount();
